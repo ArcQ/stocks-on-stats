8b96cbcac812f4db0a7610e79af8778e000c1a9c
ArcQ <eddielaw296@gmail.com>
2016-12-21 18:07:37 -0500

    feat: remove react-thunk (for redux-observables)

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,5 +1,4 @@
 import { applyMiddleware, compose, createStore } from 'redux';
-import thunk from 'redux-thunk';
 import { browserHistory } from 'react-router';
 import makeRootReducer from './reducers';
 import { updateLocation } from './location';
@@ -8,7 +7,6 @@ export default (initialState = {}) => {
   // ======================================================
   // Middleware Configuration
   // ======================================================
-  const middleware = [thunk];
 
   // ======================================================
   // Store Enhancers
@@ -31,7 +29,7 @@ export default (initialState = {}) => {
     makeRootReducer(),
     initialState,
     composeEnhancers(
-      applyMiddleware(...middleware),
+      applyMiddleware(),
       ...enhancers,
     ),
   );
