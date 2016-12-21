ae3842131b7eee49a18d5ba4bb756c323b2a223e
ArcQ <eddielaw296@gmail.com>
2016-12-21 18:07:37 -0500

    feat: remove react-thunk (for redux-observables)

--- src/routes/stock-correlation/modules/stock-correlation.js
+++ src/routes/stock-correlation/modules/stock-correlation.js
@@ -14,21 +14,12 @@ export function increment(value = 1) {
   };
 }
 
-/*  This is a thunk, meaning it is a function that immediately
-    returns a function for lazy evaluation. It is incredibly useful for
-    creating async actions, especially when combined with redux-thunk! */
-
-export const doubleAsync = () =>
-  (dispatch, getState) =>
-    new Promise((resolve) => {
-      setTimeout(() => {
-        dispatch({
-          type: COUNTER_DOUBLE_ASYNC,
-          payload: getState().counter,
-        });
-        resolve();
-      }, 200);
-    });
+export function doubleAsync(value = 1) {
+  return {
+    type: COUNTER_INCREMENT,
+    payload: value,
+  };
+}
 
 export const actions = {
   increment,
