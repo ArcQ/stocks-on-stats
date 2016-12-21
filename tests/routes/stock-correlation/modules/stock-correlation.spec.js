1e3288a3b475ca3ca292b63c376cfed4399b78d5
Eddie Law <eddielaw296@gmail.com>
2016-12-21 18:07:37 -0500

    feat: remove react-thunk (for redux-observables)

--- tests/routes/stock-correlation/modules/stock-correlation.spec.js
+++ tests/routes/stock-correlation/modules/stock-correlation.spec.js
@@ -69,44 +69,6 @@ describe('(Redux Module) Counter', () => {
       })
     })
 
-    it('Should be exported as a function.', () => {
-      expect(doubleAsync).to.be.a('function')
-    })
-
-    it('Should return a function (is a thunk).', () => {
-      expect(doubleAsync()).to.be.a('function')
-    })
-
-    it('Should return a promise from that thunk that gets fulfilled.', () => {
-      return doubleAsync()(_dispatchSpy, _getStateSpy).should.eventually.be.fulfilled
-    })
-
-    it('Should call dispatch and getState exactly once.', () => {
-      return doubleAsync()(_dispatchSpy, _getStateSpy)
-        .then(() => {
-          _dispatchSpy.should.have.been.calledOnce
-          _getStateSpy.should.have.been.calledOnce
-        })
-    })
-
-    it('Should produce a state that is double the previous state.', () => {
-      _globalState = { counter: 2 }
-
-      return doubleAsync()(_dispatchSpy, _getStateSpy)
-        .then(() => {
-          _dispatchSpy.should.have.been.calledOnce
-          _getStateSpy.should.have.been.calledOnce
-          expect(_globalState.counter).to.equal(4)
-          return doubleAsync()(_dispatchSpy, _getStateSpy)
-        })
-        .then(() => {
-          _dispatchSpy.should.have.been.calledTwice
-          _getStateSpy.should.have.been.calledTwice
-          expect(_globalState.counter).to.equal(8)
-        })
-    })
-  })
-
   // NOTE: if you have a more complex state, you will probably want to verify
   // that you did not mutate the state. In this case our state is just a number
   // (which cannot be mutated).
