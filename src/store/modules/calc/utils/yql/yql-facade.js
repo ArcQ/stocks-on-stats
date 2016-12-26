2b7939216573d75c665b5a053eca0dcd5e4ed2b7
Eddie Law <eddielaw296@gmail.com>
2016-12-26 00:55:15 -0500

    feat: move from rx-lite to rxjs for redux observables functionality

---
+++ src/store/modules/calc/utils/yql/yql-facade.js
@@ -0,0 +1,35 @@
+import YqlRequests from './yql-requests';
+import SosApiAdapter from '../sos-api-adapter';
+import { Observable } from 'rxjs/Observable';
+import Http from './http-adapter';
+import 'rxjs/add/observable/forkJoin';
+import 'rxjs/Rx';
+
+const _url = "http://localhost:5000";
+
+function _getStockCorrelation(interval, ...stockSymbols) {
+  return YqlRequests
+    .checkSymbols(stockSymbols)
+    .flatMap(() =>
+      YqlRequests.getHistoricalForStocks(interval, stockSymbols)
+    )
+    .flatMap(result =>{
+      console.log(result);
+      const url = `${_url}/calculators/stock-correlation`;
+      return Http.post(url, JSON.stringify({ "historicalData": result }));
+    }
+    )
+}
+
+const calculatorDict = {
+  'stock-correlation': _getStockCorrelation,
+};
+
+export default{
+  /* makeCalculation: function
+  description: sends request to yql then redirects response to sos api
+  {param 1}: takes location to see which calculator methods to use
+  {param 2+}: rest of parameters should match the method it is calling eg. getStockCorrelation
+  */
+  makeCalculation: (location, args) => _getStockCorrelation(...args),
+};
