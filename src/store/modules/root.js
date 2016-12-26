15b588b3ee76d724878897d0e8e8a24d1794fd2c
ArcQ <eddielaw296@gmail.com>
2016-12-26 00:55:15 -0500

    feat: move from rx-lite to rxjs for redux observables functionality

---
+++ src/store/modules/root.js
@@ -0,0 +1,36 @@
+import { combineReducers } from 'redux';
+import { combineEpics } from 'redux-observable';
+import locationReducer from './location';
+import { calcEpic, calcReducer, CALC_REQUEST } from './calc';
+
+// ------------------------------------
+// Root Epic
+// ------------------------------------
+
+const _registeredEpics = [CALC_REQUEST];
+export const rootEpic = calcEpic;
+
+export const injectEpic = (key, asyncEpic) => {
+  if (_registeredEpics.some((ele => ele === key))) return;
+  rootEpic.next(asyncEpic);
+};
+
+// ------------------------------------
+// Root Reducer
+// ------------------------------------
+
+export const makeRootReducer = asyncReducers =>
+  combineReducers({
+    location: locationReducer,
+    calc: calcReducer,
+    ...asyncReducers,
+  });
+
+export const injectReducer = (store, { key, reducer }) => {
+  if (Object.hasOwnProperty.call(store.asyncReducers, key)) return;
+
+  store.asyncReducers[key] = reducer;
+  store.replaceReducer(makeRootReducer(store.asyncReducers));
+};
+
+export default { rootEpic, makeRootReducer, injectEpic, injectReducer };
