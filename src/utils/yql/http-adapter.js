8d7e7c030b085ff7ca2d26ea4f323801b3598dc4
Eddie Law <eddielaw296@gmail.com>
2016-12-26 00:58:38 -0500

    refactor: delete files unused by new directory structure

--- src/utils/yql/http-adapter.js
+++
@@ -1,9 +0,0 @@
-import * as rx from 'rx-dom-ajax';
-
-export default {
-  jsonp: url => rx.DOM.jsonpRequest({
-    url,
-    jsonp: 'cbfunc',
-    jsonpCallback: 'callback',
-  }).catch(e => console.warn('error needs to be handled' + e)),
-};
