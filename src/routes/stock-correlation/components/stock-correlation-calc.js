0456f9dc928e256eac773808732e916eb9f0f5db
ArcQ <eddielaw296@gmail.com>
2016-12-30 16:23:55 -0500

    feat: add react-toolbox, and override css styles

--- src/routes/stock-correlation/components/stock-correlation-calc.js
+++ src/routes/stock-correlation/components/stock-correlation-calc.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Table from 'react-toolbox/lib/table';
+import theme from 'styles/twoDimensionDataTable.scss';
 
 // sample matrix: [["g", "stx"], ["g", 1.0, 0.25], ["stx", 0.26, 1.0]]
 const StockMatrix = (calcResult) => {
@@ -10,18 +12,44 @@ const StockMatrix = (calcResult) => {
   return calcResult;
 };
 
+const UserModel = {
+  "": {type: String},
+  twitter: {type: String},
+  birthdate: {type: Date},
+  cats: {type: Number},
+  dogs: {type: Number},
+  active: {type: Boolean}
+};
+
+const users = [
+  {'': 'Javi Jimenez', twitter: '@soyjavi', birthdate: new Date(1980, 3, 11), cats: 1},
+  {'': 'Javi Velasco', twitter: '@javivelasco', birthdate: new Date(1987, 1, 1), dogs: 1, active: true}
+];
+
+
 export const StockCorrelationCalc = props => {
   console.log(props.calcResult);
   if(props.isCalcResult){
     return (
       <div>
-        <h2> Results </h2>
-        { StockMatrix(props.calcResult) }
-        { [<div>test1</div>, <div>test2</div>]}
+        <div>
+          <h2> Results </h2>
+          { StockMatrix(props.calcResult) }
+          { [<div>test1</div>, <div>test2</div>]}
+        </div>
+        <div>
+          <Table
+            theme={theme}
+            className={theme.twoDimensionDataTable}
+            model={UserModel}
+            source={users}
+            selectable={false}
+          />
+        </div>
       </div>
     );
-  }
-  return null;
+}
+return null;
 };
 
 StockCorrelationCalc.propTypes = {
