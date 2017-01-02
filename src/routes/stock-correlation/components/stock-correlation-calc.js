67f04e148d39c015b86fabf18341ca3a7b8dfc9e
ArcQ <eddielaw296@gmail.com>
2017-01-02 04:34:43 -0500

    refactor: fix eslint errors

--- src/routes/stock-correlation/components/stock-correlation-calc.js
+++ src/routes/stock-correlation/components/stock-correlation-calc.js
@@ -1,34 +1,31 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import Table from 'react-toolbox/lib/table';
 import theme from 'styles/twoDimensionDataTable.scss';
 
-export const StockCorrelationCalc = props => {
-  console.log(props.calcResult);
-  if(props.isCalcResult){
-    return (
-      <div>
-        <div>
-          <h2> Results </h2>
-        </div>
-        <div>
-          <Table
-            theme={theme}
-            className={theme.twoDimensionDataTable}
-            model={props.calcResult.symbolModel}
-            source={props.calcResult.correlations}
-            selectable={false}
-          />
-        </div>
-      </div>
-    );
-}
-return null;
-};
+export const StockCorrelationCalc = (props) => (
+  <div>
+    <div>
+      <h2> Results </h2>
+    </div>
+    <div>
+      <Table
+        theme={theme}
+        className={theme.twoDimensionDataTable}
+        model={props.calcResult.symbolModel}
+        source={props.calcResult.correlations}
+        selectable={false}
+      />
+    </div>
+  </div>
+);
 
 StockCorrelationCalc.propTypes = {
-  counter: React.PropTypes.number.isRequired,
-  doubleAsync: React.PropTypes.func.isRequired,
-  increment: React.PropTypes.func.isRequired,
+  calcResult: PropTypes.shape({
+    symbolModel: PropTypes.shape({}),
+    correlations: PropTypes.arrayOf(
+      PropTypes.shape({}),
+    ),
+  }),
 };
 
 export default StockCorrelationCalc;
