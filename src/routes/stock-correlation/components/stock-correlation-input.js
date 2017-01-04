3bdea0f2aa801be00516833e93adc2e8ebeda1e4
Eddie Law <eddielaw296@gmail.com>
2017-01-04 18:32:48 -0500

    chore:fix scss loader in webpack

--- src/routes/stock-correlation/components/stock-correlation-input.js
+++ src/routes/stock-correlation/components/stock-correlation-input.js
@@ -1,37 +1,40 @@
 import React from 'react';
-import { browserHistory } from 'react-router';
+import TagsInput from 'react-tagsinput';
+import 'react-tagsinput/react-tagsinput.css';
+import './stock-correlation.scss';
 
 export const StockCorrelationInput = props => (
   <div>
-    <h2>Counter: {props.counter}</h2>
-    <button className='btn btn-default' onClick={props.increment}>
-      Increment
-    </button>
-    {' '}
-    <button className='btn btn-default' onClick={props.doubleAsync}>
-      Double (Async)
-    </button>
-    <button className='btn btn-default' onClick={() => browserHistory.push('/stock-correlation?cid=1234')}>
-      GoTo Test
-    </button>
+    <h2>Stock Correlation</h2>
     <button
       className='btn btn-default'
       onClick={
         () =>
-        props.makeCalc({
-          startDate: '2013-01-01',
-          endDate: '2013-07-01'
-        }, 'g', 'stx')
-      }>
+          props.makeCalc({
+            startDate: '2013-01-01',
+            endDate: '2013-07-01',
+          }, 'g', 'stx')
+      }
+    >
       Call
     </button>
+    <TagsInput 
+      value={props.taggedStocks}
+      addKeys={props.keyCodesForAdd}
+      onChange={props.handleTagInput}
+      onlyUnique={true}
+      inputProps={{
+        className: 'react-tagsinput-input',
+        placeholder: 'eg. GOOG;AAPL;AMZN;',
+      }}
+    />
   </div>
 );
 
 StockCorrelationInput.propTypes = {
-  counter: React.PropTypes.number.isRequired,
-  doubleAsync: React.PropTypes.func.isRequired,
-  increment: React.PropTypes.func.isRequired,
+  // eslint rule error with stateless components awaiting fix
+  // eslint-disable-next-line react/no-unused-prop-types
+  makeCalc: React.PropTypes.func.isRequired,
 };
 
 export default StockCorrelationInput;
