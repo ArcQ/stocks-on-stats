aaeb1e5dcd09a608106a53fd2ec94e6d72132863
ArcQ <eddielaw296@gmail.com>
2017-01-05 15:36:08 -0500

    feat:basic styling of stock correlation

--- src/routes/stock-correlation/components/stock-correlation-input.js
+++ src/routes/stock-correlation/components/stock-correlation-input.js
@@ -1,40 +1,66 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { Button, section, DatePicker } from 'react-toolbox';
 import TagsInput from 'react-tagsinput';
 import 'react-tagsinput/react-tagsinput.css';
 import './stock-correlation.scss';
 
+// TODO when user goes beyond 1 year on date picker, show dialog alerting user of limitation
+
 export const StockCorrelationInput = props => (
   <div>
-    <h2>Stock Correlation</h2>
-    <button
-      className='btn btn-default'
-      onClick={
-        () =>
-          props.makeCalc({
-            startDate: '2013-01-01',
-            endDate: '2013-07-01',
-          }, 'g', 'stx')
-      }
-    >
-      Call
-    </button>
-    <TagsInput 
+    <h2>Correlation Matrix</h2>
+    <TagsInput
       value={props.taggedStocks}
       addKeys={props.keyCodesForAdd}
       onChange={props.handleTagInput}
-      onlyUnique={true}
       inputProps={{
         className: 'react-tagsinput-input',
         placeholder: 'eg. GOOG;AAPL;AMZN;',
       }}
+      onlyUnique
     />
+    <section>
+      <DatePicker
+        label='Start Date'
+        onChange={props.handleStartDateInput}
+        value={props.interval.startDate}
+        minDate={props.interval.min}
+        maxDate={props.interval.max}
+        sundayFirstDayOfWeek
+      />
+      <DatePicker
+        label='End Date'
+        onChange={props.handleEndDateInput}
+        value={props.interval.endDate}
+        minDate={props.interval.min}
+        maxDate={props.interval.max}
+        sundayFirstDayOfWeek
+      />
+    </section>
+    <Button
+      className='btn btn-default'
+      onClick={() => props.makeStockCorrelationCalc()}
+      raised
+      primary
+    >
+      Calculate
+    </Button>
   </div>
 );
 
 StockCorrelationInput.propTypes = {
-  // eslint rule error with stateless components awaiting fix
-  // eslint-disable-next-line react/no-unused-prop-types
-  makeCalc: React.PropTypes.func.isRequired,
+  makeStockCorrelationCalc: PropTypes.func.isRequired,
+  keyCodesForAdd: PropTypes.arrayOf(PropTypes.number),
+  taggedStocks: PropTypes.arrayOf(PropTypes.string).isRequired,
+  interval: PropTypes.shape({
+    startDate: PropTypes.instanceOf(Date).isRequired,
+    endDate: PropTypes.instanceOf(Date).isRequired,
+    min: PropTypes.instanceOf(Date).isRequired,
+    max: PropTypes.instanceOf(Date).isRequired,
+  }).isRequired,
+  handleStartDateInput: PropTypes.func.isRequired,
+  handleEndDateInput: PropTypes.func.isRequired,
+  handleTagInput: PropTypes.func.isRequired,
 };
 
 export default StockCorrelationInput;
